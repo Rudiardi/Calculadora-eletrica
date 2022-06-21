7bad15b14ccbdae3dcac78eb82a7755b618a88a3
Rudiardi <rudi.junior@hotmail.com>
2022-06-21 19:42:46 -0300

    commit vscode

--- src/componentes/Contents/index.js
+++ src/componentes/Contents/index.js
@@ -1,26 +1,69 @@
-import React, {Component} from 'react';
+import React, {useState, useRef} from 'react';
 import { 
     View,
     Text,
     StyleSheet,
     TextInput,
-    
  } from 'react-native';
 
-export default function Contents({bitola,disjutor}) {
+import DropDownPicker from 'react-native-dropdown-picker';
+
+//import {Picker} from '@react-native-picker/picker';
+//react select
+
+
+export default function Contents({bitola,disjutor}) { 
+    const [open, setOpen] = useState(false);
+    const [value, setValue] = useState(0.0172);
+    const [items, setItems] = useState([
+        {label: 'Cobre', value: 0.0172},
+        {label: 'Aluminio', value: 0.0282}
+      ]);
+
+    const [open2, setOpen2] = useState(false);
+    const [value2, setValue2] = useState(0.01);
+    const [items2, setItems2] = useState([
+        {label: '1%', value: 0.01},
+        {label: '2%', value: 0.02},
+        {label: '3%', value: 0.03},
+        {label: '4%', value: 0.04},
+        {label: '5%', value: 0.05},
+      ]);
+
+
+    
+
  return (
     <View style={styles.total}>
         <View style={styles.container}>
             <View style={styles.subcontainer}>
                 <View style={styles.organizaTexto}>
                     <Text style={styles.titulos}>Material</Text>
-                    <TextInput style={styles.inputs}/>
+                    <DropDownPicker 
+                        dropDownStyle={{fontWeight:'bold'}}
+                        style={styles.dropDown}
+                        open={open}
+                        value={value}
+                        items={items}
+                        setOpen={setOpen}
+                        setValue={setValue}
+                        setItems={setItems}
+                    />
                 </View>       
             </View>
             <View style={styles.subcontainer}>
                 <View style={styles.organizaTexto}>
                     <Text style={styles.titulos}>Queda de Tensão (%)</Text>
-                    <TextInput style={styles.inputs} keyboardType={'number-pad'}/>
+                    <DropDownPicker 
+                        dropDownStyle={{fontWeight:'bold'}}
+                        style={styles.dropDown}
+                        open={open2}
+                        value={value2}
+                        items={items2}
+                        setOpen={setOpen2}
+                        setValue={setValue2}
+                        setItems={setItems2}
+                    />
                 </View>
             </View>
         </View>
@@ -56,13 +99,15 @@ export default function Contents({bitola,disjutor}) {
             <View style={styles.organizaTexto}>
                 <Text style={styles.titulos}>Disjuntor recomedado (A)</Text>
                 <View style={styles.outputSingle}>
-                    <Text style={styles.result}>{disjutor}</Text>
+                    <Text style={styles.result}></Text>
                 </View>
             </View>
         </View>
     </View>
 );
 }
+
+
 
 const styles = StyleSheet.create({
     container:{
@@ -107,7 +152,13 @@ const styles = StyleSheet.create({
         paddingLeft:60
     },
     dropDown:{
-        width:80
+        backgroundColor:'#4C3B4D',
+        borderWidth:1,
+        borderColor:'#A53860',
+        borderRadius: 10,
+        width:180,
+        height:50,
+        paddingLeft:60,
     },
     result:{
         color:'#FFEEDB',
@@ -124,5 +175,12 @@ const styles = StyleSheet.create({
         height:150,
         justifyContent:'center',
         alignItems:'center'
+    },
+    listaSusp:{
+        height:50,
+        width:200,
+        backgroundColor:'#4C3B4D',
+        color:'#FFEEDB',
+        fontWeight:'bold',
     }
 })
